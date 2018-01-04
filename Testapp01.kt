7654d6fa103330db438f65c5e35ffce80dc4a805
KKKKK <applay.27@gmail.com>
2018-01-04 19:05:16 +0900

    Initial commit

---
+++ Testapp01.kt
@@ -0,0 +1,12 @@
+package com.example.kkkkk.test_app_01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Testapp01 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testapp01)
+    }
+}
